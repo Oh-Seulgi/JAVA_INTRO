d936b3832c4159c30311bf591dded7df609b9f00
sgoh <sgoh@bizdata.kr>
2024-01-05 09:56:16 +0900

    메서드 오버로딩 설명 추가

--- src/method/Overloading1.java
+++ src/method/Overloading1.java
@@ -1,5 +1,9 @@
 package method;
 
+/*
+*  메서드 오버로딩
+*   - 메서드의 이름이 같아도 매개변수의 타입 및 순서가 다르면 오버로딩을 할 수 있다. 참고로 반환 타입은 인정하지 않는다.
+* */
 public class Overloading1 {
 
     public static void main(String[] args) {
